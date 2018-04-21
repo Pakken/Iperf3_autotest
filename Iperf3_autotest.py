3846f2938b005aa067e6dc723640b333bf11f42c
Pakken <pavel.isk.ne@gmail.com>
2018-04-21 21:34:53 +0300

    complete version but without some usefull things

--- Iperf3_autotest.py
+++ Iperf3_autotest.py
@@ -1,24 +1,22 @@
 from subprocess import call
-from time import sleep
 from datetime import datetime
 
 IPERF3_SERV = '127.0.0.2'
-TIME_TEST_VAR = '20'
-BYTES_TEST_VAR = '10000000000'# Gigabytes
+TIME_TEST_VAR = '100'
+BYTES_TEST_VAR = '100000000000'# Gigabytes
 #PACKETS_TEST_VAR = str(2*10^9) # Gigapackets
 MAX_FLOW_NUM = 2
 TIMESTAMP = str(10)
 TESTS = [['iperf3','-c',IPERF3_SERV,'-i',TIMESTAMP,'-t',TIME_TEST_VAR,'-J','-P'],
          ['iperf3','-c',IPERF3_SERV,'-i',TIMESTAMP,'-n',BYTES_TEST_VAR,'-J','-P']]
-         #['iperf3','-c',IPERF3_SERV,'-i',TIMESTAMP,'-k',PACKETS_TEST_VAR,'-J','-P']]
 
 FILE_NAME = 'iperf3_test_{}_{}'
 
 
 def main():
-    with open('results.txt','a+') as results :
+    with open('results','a+') as results :
         for i in TESTS:
-            res_file_name = FILE_NAME.format(datetime.today(),i[5])
+            res_file_name = FILE_NAME.format(datetime.today().strftime('%H:%M_%d:%m:%Y'),i[5])
             results.writelines(res_file_name + '\n')
             with open(res_file_name,'w+') as f:
                 for j in range(MAX_FLOW_NUM):
@@ -34,7 +32,6 @@ def main():
                         i.remove(str(j+1))
                     except:
                         print('Error')
-                    
 
 if __name__ == '__main__':
     main()
--- Process_results.py
+++ Process_results.py
@@ -1,5 +1,5 @@
 import yaml 
-import matplotlib
+import matplotlib.pyplot as plt
 
 ##with open('results.txt','r') as results:
 ##    for i in results.readlines():
@@ -14,22 +14,64 @@ clear_data = []
 
 
 def main():
-    with open('iperf3-t','r') as data:
-        try:
-            d = data.read().replace('\t',' ').replace('}\n{','}\nUUUFFF\n{').split('UUUFFF') # prepare data
-            for i in d:         # populate clear_data
-                clear_data.append(yaml.load(i))
-            for i in clear_data:    # test output 
-                print(i['start']['test_start'])
+    with open('results','r') as results:
+        for result in results.readlines():
+            with open(result.strip(),'r') as data:
+                try:
+                    d = data.read().replace('\t',' ').replace('}\n{','}\nUUUFFF\n{').split('UUUFFF') # prepare data
+                    for i in d:         # populate clear_data
+                        clear_data.append(yaml.load(i))
+        ##            for i in clear_data:    # test output 
+        ##                print(i['start']['test_start'])
+        ##                for j in i['intervals']:
+        ##                    print(j['sum']['start'])
+        ##                    print(j['sum']['end'])
+        ##                    print(j['sum']['bits_per_second'])
+        ##                print(i['end']['sum_sent']['start'])
+        ##                print(i['end']['sum_sent']['end'])
+                except yaml.YAMLError as exc:
+                    print(exc)
+
+            for i in clear_data:
+                time_ticks = []
+                bps_ticks = []
+                # create special title
+                head_title = ['TEST RESULTS WITH IPERF3 PROGRAM',
+                              'TEST INFO :',
+                              #'system info : ' + i['start']['system_info'],
+                              'test proto : ' + i['start']['test_start']['protocol'],
+                              'num of streams : ' + str(i['start']['test_start']['num_streams']),
+                              'reverse mode : ' + str(i['start']['test_start']['reverse'])]
+                text = ['SUMMARY INFO :',
+                          'Sent :',
+                          'duration in sec : ' + str(i['end']['sum_sent']['seconds']),
+                          'total bytes : ' + str(i['end']['sum_sent']['bytes']),
+                          'average speed : ' + str(i['end']['sum_sent']['bits_per_second']),
+                          'count of retransmits : ' + str(i['end']['sum_sent']['retransmits']),
+                          'Received :',
+                          'duration in sec : ' + str(i['end']['sum_received']['seconds']),
+                          'total bytes : ' + str(i['end']['sum_received']['bytes']),
+                          'average speed : ' + str(i['end']['sum_received']['bits_per_second'])]
+                # create data for plot
                 for j in i['intervals']:
-                    print(j['sum']['start'])
-                    print(j['sum']['end'])
-                    print(j['sum']['bits_per_second'])
-                print(i['end']['sum_sent']['start'])
-                print(i['end']['sum_sent']['end'])
-        except yaml.YAMLError as exc:
-            print(exc)
+                    time_ticks.append(j['sum']['end'])
+                    bps_ticks.append(j['sum']['bits_per_second'])
 
-
+                fig = plt.figure(figsize=(10,15))
+                ax = fig.add_subplot(111)
+                fig.subplots_adjust(top=0.6)
+                ax.set_title('\n'.join(head_title) + '\n'.join(text),fontsize=14)
+                ax.set_xlabel('time in sec')
+                ax.set_ylabel('bits per second')
+                ax.grid()
+                ax.bar(time_ticks,
+                        bps_ticks,
+                        5,
+                        color = [0.1,0.5,0.5],
+                        align = 'center')
+                plt.savefig('iperf3_test_{}_{}_{}.pdf'.format(i['start']['timestamp']['time'],
+                                                              i['start']['test_start']['num_streams'],
+                                                              i['start']['test_start']['reverse']))
+        
 if __name__ == '__main__':
     main()
